2b82cd15481451b0d15b43f15155456352fb84e5
hfuent761 <hfuent761@westmec.org>
2024-09-26 14:19:21 -0700

    updated project

--- server.js
+++ server.js
@@ -1,3 +1,43 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs').promises;
+
+const app = express();
+
+const clientPath = path.join(__dirname, '..', 'client/src');
+const dataPath = path.join(__dirname, 'data', 'users.json');
+const serverPublic = path.join(__dirname, 'public');
+
+// Middleware setup
+app.use(express.static(clientPath)); // Serve static files from client directory
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(express.json()); // Parse JSON bodies
+
+// home route
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: clientPath });
+});
+
+app.get('/superheroes', async (req, res) => {
+    try {
+        const data = await fs.readFile(dataPath, 'utf8');
+
+        const users = JSON.parse(data);
+        if (!users) {
+            throw new Error("Error no users available");
+        }
+        res.status(200).json(users);
+    } catch (error) {
+        console.error("Problem getting users" + error.message);
+        res.status(500).json({ error: "Problem reading users" });
+    }
+});
+
+//to post
+app.post('/submit-form', async (req, res) => {
+    try {
+        const { heros, powers } = req.body
+    } catch (error) {
+
+    }
+})
